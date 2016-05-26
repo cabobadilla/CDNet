17682140106fcca8e12e87bd1d31c3c89bfe78e1
luisfronquillo <feval99@gmail.com>
2016-05-26 16:56:41 -0500

    inicial

---
+++ NetProjTest/WebApplication1/Default.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+
+namespace WebApplication1
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        public double num { get; set; } 
+        public void Page_Load(object sender, EventArgs e)
+        {
+            Random a = new Random();
+
+            num = a.Next()+10;
+            
+
+        }
+       
+
+        
+
+    }
+}
